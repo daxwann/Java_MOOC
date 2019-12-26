9b843fb05d1f72cd592ccdd54c63d2a818ea4c7d
Dax Wann <daxwann@gmail.com>
2019-12-25 22:17:37 -0800

    complete 1.14-1.21

--- week1-014.PositiveValue/src/PositiveValue.java
+++ week1-014.PositiveValue/src/PositiveValue.java
@@ -7,5 +7,13 @@ public class PositiveValue {
         Scanner reader = new Scanner(System.in);
 
         // Type your program here:
+        System.out.print("Type a number: ");
+        int num = Integer.parseInt(reader.nextLine());
+
+        if (num > 0) {
+            System.out.println("The number is positive");
+        } else {
+            System.out.println("The number is not positive");
+        }
     }
 }
--- week1-018.GradesAndPoints/src/GradesAndPoints.java
+++ week1-018.GradesAndPoints/src/GradesAndPoints.java
@@ -5,6 +5,20 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        System.out.print("Type the points [0-60]: ");
+        int score = Integer.parseInt(reader.nextLine());
+        if (score >= 0 && score <= 29) {
+            System.out.println("\nGrade: failed");
+        } else if (score >= 30 && score <= 34) {
+            System.out.println("\nGrade: 1");
+        } else if (score >= 35 && score <= 39) {
+            System.out.println("\nGrade: 2");
+        } else if (score >= 40 && score <= 44) {
+            System.out.println("\nGrade: 3");
+        } else if (score >= 45 && score <= 49) {
+            System.out.println("\nGrade: 4");
+        } else if (score >= 50 && score <= 60) {
+            System.out.println("\nGrade: 5");
+        }
     }
 }

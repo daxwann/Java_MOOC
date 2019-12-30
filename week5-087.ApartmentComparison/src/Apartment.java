159042f2e212c8b627fda28c225a18f4561b78ee
Dax Wann <daxwann@gmail.com>
2019-12-30 13:10:34 -0800

    complete week5 ex87

--- week5-087.ApartmentComparison/src/Apartment.java
+++ week5-087.ApartmentComparison/src/Apartment.java
@@ -10,5 +10,25 @@ public class Apartment {
         this.squareMeters = squareMeters;
         this.pricePerSquareMeter = pricePerSquareMeter;
     }
-    
+
+    public boolean larger(Apartment otherApt) {
+        return this.squareMeters > otherApt.squareMeters;
+    }
+
+    public int priceDifference(Apartment otherApt) {
+        int price1 = this.pricePerSquareMeter * this.squareMeters;
+        int price2 = otherApt.pricePerSquareMeter * otherApt.squareMeters;
+        int priceDiff = price1 - price2;
+        if (priceDiff < 0) {
+            return -1 * priceDiff;
+        }
+        return priceDiff;
+    }
+
+    public boolean moreExpensiveThan(Apartment otherApt) {
+        int priceSelf = this.pricePerSquareMeter * this.squareMeters;
+        int priceOther = otherApt.pricePerSquareMeter * otherApt.squareMeters;
+
+        return priceSelf > priceOther;
+    }
 }

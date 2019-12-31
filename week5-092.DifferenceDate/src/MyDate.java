017fb619b636c2ac15ea290375596f679b4664ef
Dax Wann <daxwann@gmail.com>
2019-12-31 11:45:14 -0800

    complete week5 ex92

--- week5-092.DifferenceDate/src/MyDate.java
+++ week5-092.DifferenceDate/src/MyDate.java
@@ -30,4 +30,19 @@ public class MyDate {
         return false;
     }
 
+    public int differenceInYears(MyDate comparedDate) {
+        int diffYear = this.year - comparedDate.year;
+        int diffMonth = this.month - comparedDate.month;
+        int diffDay = this.day - comparedDate.day;
+
+        int exactDiffDay = diffYear * 360 + diffMonth * 30 + diffDay;
+
+        int exactDiffYear = exactDiffDay / 360;
+
+        if (exactDiffYear < 0) {
+            exactDiffYear *= -1;
+        }
+
+        return exactDiffYear;
+    }
 }
